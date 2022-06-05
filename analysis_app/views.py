f8814cecb328079f4e971b40d3367e1381b5d65e
r4nd0lph <r11345roma@yandex.ru>
2022-06-05 23:53:27 +0300

    created app & created handler 404 & added media root

--- analysis_app/views.py
+++ analysis_app/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Страница приложения analysis</h1>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
--- moscityhack_2022/urls.py
+++ moscityhack_2022/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from analysis_app.views import page_not_found
+from moscityhack_2022 import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('analysis_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = page_not_found
